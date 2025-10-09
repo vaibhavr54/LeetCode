6e5a5f5a02c5454b05d240d8c7bc7a39e1c9b193
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2025-10-09 21:58:35 +0530

    Time: 2 ms (77.55%), Space: 7.8 MB (63.72%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
+        // if (n==0 || n==1) return n;
+        // return fib(n-1)+fib(n-2);
         if (n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        int a=0, b=1;
+        for (int i=2; i<=n; i++) {
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 };
