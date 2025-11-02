ad0b545f1bd893e72d5ef0faa88a0bcd56dbd9b7
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2025-11-02 21:21:08 +0530

    Time: 6 ms (63.55%), Space: 139 MB (21.94%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> result;
+        // vector<int> result;
+
+        // for (int num : nums) {
+        //     if (num < pivot) {
+        //         result.push_back(num);
+        //     }
+        // }
+
+        // for (int num : nums) {
+        //     if (num == pivot) {
+        //         result.push_back(num);
+        //     }
+        // }
+
+        // for (int num : nums) {
+        //     if (num > pivot) {
+        //         result.push_back(num);
+        //     }
+        // }
+
+        // return result;
+
+        vector<int> less, equal, greator;
 
         for (int num : nums) {
-            if (num < pivot) {
-                result.push_back(num);
-            }
+            if (num < pivot) less.push_back(num);
+            else if (num == pivot) equal.push_back(num);
+            else if (num > pivot) greator.push_back(num);
         }
+        less.insert(less.end(), equal.begin(), equal.end());
+        less.insert(less.end(), greator.begin(), greator.end());
 
-        for (int num : nums) {
-            if (num == pivot) {
-                result.push_back(num);
-            }
-        }
-
-        for (int num : nums) {
-            if (num > pivot) {
-                result.push_back(num);
-            }
-        }
-
-        return result;
+        return less;
     }
 };
