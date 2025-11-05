504179a34e3252a9a608b992f0e793cb6fa0e778
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2025-11-05 19:09:06 +0530

    Time: 0 ms (100%), Space: 13 MB (56.34%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
+        // int n = sentences.size();
+        // int maxWords = 0;
+        // for (int i=0; i<n; i++) {
+        //     int currCnt = 1;
+        //     for (int j=0; j<sentences[i].size(); j++) {
+        //         if (sentences[i][j] == ' ') {
+        //             currCnt++;
+        //         }
+        //     }
+        //     maxWords = max(maxWords, currCnt);
+        // }
+        // return maxWords;
+
         int n = sentences.size();
         int maxWords = 0;
-        for (int i=0; i<n; i++) {
-            int currCnt = 1;
-            for (int j=0; j<sentences[i].size(); j++) {
-                if (sentences[i][j] == ' ') {
-                    currCnt++;
-                }
-            }
-            maxWords = max(maxWords, currCnt);
+        for (auto& s : sentences) {
+            int currWords = count(s.begin(), s.end(), ' ') + 1;
+            maxWords = max(maxWords, currWords);
         }
         return maxWords;
     }
