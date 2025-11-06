9599875686d868def3983b424195cba8abe64b4e
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2025-11-06 23:52:02 +0530

    Time: 3 ms (46.97%), Space: 7.9 MB (65%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfMatches(int n) {
         int matches = 0;
 
-        while (n != 1) {
+        while (n > 1) {
             if (n%2==0) {
                 matches += n / 2;
                 n /= 2;
