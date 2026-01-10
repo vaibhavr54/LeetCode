4b75ae7d9aa3a54a7f9f96a658e4ba5167b246a8
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-10 17:43:55 +0530

    Time: 0 ms (100%), Space: 28 MB (98.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.rbegin(), nums.rend());
-        return nums[nums.size()/2];
+        // sort(nums.rbegin(), nums.rend());
+        // return nums[nums.size()/2];
+
+        int freq = 0, ans = 0;
+        for (int num : nums) {
+            if (freq == 0) {
+                ans = num;
+            } 
+            if (num == ans) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return ans;
     }
 };
