91c368b3c60cc0f4979515e0131661d9c4a6d44d
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-12 00:54:14 +0530

    Time: 0 ms (100%), Space: 17.8 MB (58.61%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,31 +2,15 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
-        // int maxProd = nums[0], minProd = nums[0], result = nums[0];
-        // for (int i=1; i<n; i++) {
-        //     if (nums[i] < 0) {
-        //         swap(maxProd, minProd);
-        //     }
-        //     maxProd = max(nums[i], maxProd*nums[i]);
-        //     minProd = min(nums[i], minProd*nums[i]);
+        int prefix = 1, suffix = 1, ans = INT_MIN;
+        for (int i=0; i<nums.size(); i++) {
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
 
-        //     result = max(result, maxProd);
-        // }
-        // return result;
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
 
-
-        int pref = 1, suff = 1, ans = INT_MIN;
-
-        for (int i=0; i<n; i++) {
-
-            if (pref == 0) pref = 1;
-            if (suff == 0) suff = 1;
-
-            pref *= nums[i];
-            suff *= nums[n-i-1];
-
-            ans = max(ans, max(pref, suff)); 
+            ans = max(ans, max(prefix, suffix));
         }
         return ans;
     }
