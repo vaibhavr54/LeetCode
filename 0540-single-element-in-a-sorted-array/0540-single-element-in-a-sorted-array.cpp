31fbfb4690c0c93cead79a145f4b8ed7626f9df6
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-12 11:57:50 +0530

    Time: 0 ms (100%), Space: 26.3 MB (6.78%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 1) return arr[0];
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int st = 0, end = n-1;
+        if (n==1) return nums[0];
 
-        int st=0, end=n-1;
         while (st<=end) {
-            int mid = st + (end-st) / 2;
+            int mid = st + (end-st)/2;
 
-            if (mid == 0 && arr[mid] != arr[mid+1]) return arr[0];
-            if (mid == n-1 && arr[n-1] != arr[n-2]) return arr[n-1];
-            if (arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+            if (mid == 0 && nums[0] != nums[1]) return nums[0];
+            if (mid == n-1 && nums[n-1] != nums[n-2]) return nums[n-1];
+            if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
 
-            if (mid % 2 == 0) { //even halfs
-                if (arr[mid] == arr[mid-1]) {
-                    end = mid - 1;
+            if (mid % 2 == 0 ) { // even halfs
+                if (nums[mid] == nums[mid-1]) {
+                    end = mid-1;
                 } else {
-                    st = mid + 1;
+                    st = mid+1;
                 }
             } else { // odd halfs
-                if (arr[mid] == arr[mid-1]) {
-                    st = mid + 1;
+                if (nums[mid] == nums[mid-1]) {
+                    st = mid+1;
                 } else {
-                    end = mid - 1;
+                    end = mid-1;
                 }
             }
         }
