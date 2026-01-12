c494c5f5db797ca2f58f89a2ecceb111541f45a4
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-12 16:04:37 +0530

    Time: 0 ms (100%), Space: 9.9 MB (54.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,10 +3,11 @@ public:
     bool isPalindrome(string s) {
         int n = s.size();
         int st = 0, end = n-1;
-        while (st < end) {
+
+        while (st<end) {
             while (st < end && !isalnum(s[st])) st++;
             while (st < end && !isalnum(s[end])) end--;
-            if (tolower(s[st]) != tolower(s[end])) return false;
+            if (st <= end && tolower(s[st]) != tolower(s[end])) return false;
             st++, end--;
         }
         return true;
