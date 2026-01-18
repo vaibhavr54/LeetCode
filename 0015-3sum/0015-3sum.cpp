cb2c65e90422af4a890cb44491524a6de9193144
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-18 13:28:01 +0530

    Time: 43 ms (83.65%), Space: 29.1 MB (70.55%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,10 +6,12 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i=0; i<n; i++) {
+            if (i>0 && nums[i] == nums[i-1]) continue;
             int j = i+1, k = n-1;
-            while (j<k) {
-                //if (i>0 && nums[i] == nums[i-1]) continue;
+
+            while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
+
                 if (sum < 0) {
                     j++;
                 } else if (sum > 0) {
