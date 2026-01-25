08bc5b3248a5b5ff46dde7981e0b4d721d1e171a
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-25 18:16:21 +0530

    Time: 0 ms (100%), Space: 25.9 MB (92.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // int n = height.size();
+        // vector<int> lmax(n, -1);
+        // vector<int> rmax(n, -1);
+
+        // lmax[0] = height[0];
+        // rmax[n-1] = height[n-1];
+
+        // for(int i=1; i<n; i++) {
+        //     lmax[i] = max(lmax[i-1], height[i]);
+        // }
+        // for (int i=n-2; i>=0; i--) {
+        //     rmax[i] = max(rmax[i+1], height[i]);
+        // }
+        // int ans = 0;
+        // for (int i=0; i<n; i++) {
+        //     ans += min(lmax[i], rmax[i]) - height[i];
+        // }
+        // return ans;
+
         int n = height.size();
-        vector<int> lmax(n, -1);
-        vector<int> rmax(n, -1);
-
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
-
-        for(int i=1; i<n; i++) {
-            lmax[i] = max(lmax[i-1], height[i]);
-        }
-        for (int i=n-2; i>=0; i--) {
-            rmax[i] = max(rmax[i+1], height[i]);
-        }
+        int l = 0, r = n-1;
+        int lmax = 0, rmax = 0;
         int ans = 0;
-        for (int i=0; i<n; i++) {
-            ans += min(lmax[i], rmax[i]) - height[i];
+
+        while (l < r) {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+
+            if (lmax < rmax) {
+                ans += lmax - height[l];
+                l++;
+            } else {
+                ans += rmax - height[r];
+                r--;
+            }
         }
         return ans;
     }
