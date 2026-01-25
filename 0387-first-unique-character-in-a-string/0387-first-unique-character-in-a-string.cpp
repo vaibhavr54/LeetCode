4e81d9f3270d4411c2706d5cdba25872df6c3c14
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-01-25 22:29:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        unordered_map<char, int> mp;
+        queue<char> q;
+
+        for (int i=0; i<n; i++) {
+            if (mp.find(s[i]) == mp.end()) {
+                q.push(i);
+            }
+            mp[s[i]]++;
+        }
+        while (!q.empty() && mp[s[q.front()]] > 1) {
+            q.pop();
+        }
+        return q.empty() ? -1 : q.front();
+    }
+};
